7ffcb3373aaa0fd6f44bace8fe83267fa2b8ad77
stehdias00 <132912185+stehdias00@users.noreply.github.com>
2023-05-10 09:35:04 -0300

    Update and rename index.html to index.php

---
+++ index.php
@@ -0,0 +1,26 @@
+<html> 
+<head>
+  <title>abc</title>
+  <meta charset="utf-8"/>
+</head>
+
+<body>
+
+<h1></h1>
+
+<ul>
+<li>segue a lista de alguns motivos</li
+</ul>
+<ol>
+  <li>a</li>
+  <li>b</li>
+  <li>c</li>
+  <li>d</li>
+  <li>e</li>
+</ol>
+  <ul>
+    <li>abc</li>
+    <ol>
+
+</body>
+</html>
