88fdafe4baa0452c5bee7aebf41205e0d4f70c18
stehdias00 <132912185+stehdias00@users.noreply.github.com>
2023-05-10 09:39:24 -0300

    Update index.php

--- index.php
+++ index.php
@@ -6,21 +6,10 @@
 
 <body>
 
-<h1></h1>
-
-<ul>
-<li>segue a lista de alguns motivos</li
-</ul>
-<ol>
-  <li>a</li>
-  <li>b</li>
-  <li>c</li>
-  <li>d</li>
-  <li>e</li>
-</ol>
-  <ul>
-    <li>abc</li>
-    <ol>
+<?php
+  echo "olá, turma do az900" ; 
+  
+  ?>
 
 </body>
 </html>
