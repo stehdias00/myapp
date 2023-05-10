9fc5d6a03077350a28e815231382156325bc9c6d
stehdias00 <132912185+stehdias00@users.noreply.github.com>
2023-05-10 12:01:33 -0300

    Update index.php

--- index.php
+++ index.php
@@ -9,6 +9,10 @@
     <p>Bem vindo a minha página inicial! Aqui voce encontrará informaçoes sobre mim e meus interresses.</p>
     <p>Sou uma aluna iniciante em programação e cloud computing.</p>
     <p>Gosto também de assistir filmes, jogar volei, fazer musculação e estudar.</p>
+    <?php
+    date_default_timezone_set('America/Sao_Paulo');
+    echo date('h:i:s A');
+    ?>
     <a href="#" class="button">Entre em contato</a>
   </body>
 </html>
