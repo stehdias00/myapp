de713087e3245506061e9a49cee57ad728dd42d2
stehdias00 <132912185+stehdias00@users.noreply.github.com>
2023-05-10 10:44:50 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,13 @@
-<html> 
-<head>
-  <title>abc</title>
-  <meta charset="utf-8"/>
-</head>
-
-<body>
-
-<?php
-  echo "olá, turma do az900" ; 
-  
-  ?>
-
-</body>
+<!doctype html>
+<html>
+  <head>
+    <title>Minha página inicial</title>
+  </head>
+  <body>
+    <h1>Minha página inicial</h1>
+    <p>Bem vindo a minha página inicial! Aqui voce encontrará informaçoes sobre mim e meus interresses.</p>
+    <p>Sou uma aluna iniciante em programação e cloud computing.</p>
+    <p>Gosto também de assistir filmes, jogar volei, fazer musculação e estudar.</p>
+    <a href="#" class="button">Entre em contato</a>
+  </body>
 </html>
